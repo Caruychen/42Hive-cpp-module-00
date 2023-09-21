fd6e792b8cb1bf0f51e40ca31591940c0b2bb86a
Caruy Chen <caruychen@gmail.com>
2023-09-21 18:10:53 +0300

    contact class

--- ex01/includes/Contact.hpp
+++ ex01/includes/Contact.hpp
@@ -2,9 +2,26 @@
 # define CONTACT_HPP
 
 class Contact {
+  private:
+    std::string _firstName;
+    std::string _lastName;
+    std::string _nickName;
+    std::string _phoneNumber;
+    std::string _darkestSecret;
+
   public:
     Contact(void);
     ~Contact(void);
+    std::String getFirstName(void) const;
+    std::String getLastName(void) const;
+    std::String getNickName(void) const;
+    std::String getPhoneNumber(void) const;
+    std::String getDarkestSecret(void) const;
+    void setFirstName(std::String firstName);
+    void setLastName(std::String lastName);
+    void setNickName(std::String nickName);
+    void setPhoneNumber(std::String phoneNumber);
+    void setDarkestSecret(std::String darkestSecret);
 };
 
 #endif
--- ex01/src/Contact.cpp
+++ ex01/src/Contact.cpp
@@ -10,3 +10,53 @@ Contact::~Contact(void) {
   std::cout << "Contact destroyed." << std::endl;
   return;
 }
+
+std::String getFirstName(void) const
+{
+  return (this->_firstName);
+}
+
+std::String getLastName(void) const
+{
+  return (this->_lastName);
+}
+
+std::String getNickName(void) const
+{
+  return (this->_nickName);
+}
+
+std::String getPhoneNumber(void) const
+{
+  return (this->_phoneNumber);
+}
+
+std::String getDarkestSecret(void) const
+{
+  return (this->_darkestSecret);
+}
+
+void setFirstName(std::String firstName)
+{
+  this->_firstName = firstName;
+}
+
+void setLastName(std::String lastName)
+{
+  this->_lastName = lastName;
+}
+
+void setNickName(std::String nickName)
+{
+  this->_nickName = nickName;
+}
+
+void setPhoneNumber(std::String phoneNumber)
+{
+  this->_phoneNumber = phoneNumber;
+}
+
+void setDarkestSecret(std::String darkestSecret)
+{
+  this->_darkestSecret = darkestSecret;
+}
